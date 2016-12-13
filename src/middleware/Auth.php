34aef174ac694d7e807040f9adb03e0f9631c691
tflight <tflight@gmail.com>
2016-12-13 08:54:49 -0500

    Initial commit.

---
+++ src/middleware/Auth.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Router;
+
+class Auth
+{
+    protected $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Authn middleware invokable class
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+    **/
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        $loggedIn = $_SESSION['isLoggedIn'];
+        if ($loggedIn != 'yes') {
+        	// If the user is not logged in, redirect them home
+            return $response->withRedirect($this->router->pathFor('home'), 403);
+        }
+
+		// The user must be logged in, so just pass this request down the middleware chain.
+        $response = $next($request, $response);
+
+        // And pass the request back up the middleware chain.
+        return $response;
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,34 @@
+<?php
+// Routes
+
+$app->get('/', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'index.phtml', $args);
+})->setName('home');
+
+$app->post('/login', function ($request, $response, $args) {
+	// Check if the user's credentials are valid, then log them in however you keep track of users.
+	// Obviously this method isn't secure, and is just used to illustrate the flow.
+	if ($request->getParsedBodyParam('name') === 'user') {
+		$_SESSION['isLoggedIn'] = 'yes'; 
+		session_regenerate_id();
+		// Login success, redirect to the dashboard.
+		return $response->withRedirect($this->router->pathFor('dashboard'));
+	}
+	// Login failed, redirect home.
+	return $response->withRedirect($this->router->pathFor('home'), 403);
+});
+
+$app->get('/dashboard', function ($request, $response, $args) {
+	// The user asked for the dashboard, give it to them, but add our Auth middleware to validate.
+    return $this->renderer->render($response, 'dashboard.phtml', $args);
+})->setName('dashboard')->add('Auth');
+
+$app->get('/logout', function ($request, $response, $args) {
+	unset($_SESSION['isLoggedIn']);
+	session_regenerate_id();
+	
+	// You wouldn't typically redirect to the dashboard, just doing it to prove we are logged out!
+	// After redirecting to the dashboard, the middleware will detect the user is not logged in
+	// and then redirect to 'home'
+    return $response->withRedirect($this->router->pathFor('dashboard'));
+});
